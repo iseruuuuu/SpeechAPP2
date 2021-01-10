fad021cadac1b5c1beb24ae862bca831c0372b85
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2021-01-10 21:16:28 +0900

    TextFieldに文字を入れてボタンを押すと、喋ってくれます。

--- SpeechAPP2/ViewController.swift
+++ SpeechAPP2/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let speechService = SpeechService()
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func start(_ sender: Any) {
+        speechService.say(textField.text!)
+    }
+    
+    
+    @IBAction func stop(_ sender: Any) {
+        speechService.stop()
+    }
+    
 }
 
